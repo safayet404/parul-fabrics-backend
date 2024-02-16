957a48d856c608e09dd65b42e658a5b58b6ebea1
Safayet <hossainsafayet187@gmail.com>
2024-02-16 19:54:45 +0600

    for deploying in vercel

---
+++ models/dailyExpense.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose'); // Erase if already required
+
+// Declare the Schema of the Mongo model
+var dailyExpenseSchema = new mongoose.Schema({
+    date:{
+        type:Date,
+        required:true,
+    },
+    purpose:{
+        type:String,
+        required:true,
+    },
+    amount:{
+        type:Number,
+        required:true,
+    },
+});
+
+//Export the model
+module.exports = mongoose.model('Expense', dailyExpenseSchema);
